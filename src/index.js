4501d351bc367558f108009eb22637a6376a4ce8
Tien Hoang <duongtienhoang2002@gmail.com>
2024-01-01 09:38:17 +0700

    test lan 14

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 const queryClient = new QueryClient()
 
 root.render(
-  // <React.StrictMode>
+  <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
@@ -24,7 +24,7 @@ root.render(
       </Provider>
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
-  //  </React.StrictMode> 
+   </React.StrictMode> 
 );
 
 // If you want to start measuring performance in your app, pass a function
